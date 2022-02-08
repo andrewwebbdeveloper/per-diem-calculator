dedaaa8a9a4aa44232657ac21155583c761604ab
Andrew <awebb@ultralinq.com>
2022-02-08 17:18:33 -0500

    add functionality surrounding achieving required data structure.

--- src/calculatePerDiem.js
+++ src/calculatePerDiem.js
@@ -1,20 +1,18 @@
 // Data structures
 
-/*
-Cities
-[
+
+const cities = [
     {
-        title: low,
+        title: 'low',
         travel: 45,
-        full:  75,
+        full: 75,
     },
     {
-        title: high,
+        title: 'high',
         travel: 55,
-        full:  85,
+        full: 85,
     },
 ]
-*/
 
 /*
 Set (input)
@@ -34,6 +32,52 @@ Set (input)
 ]
 */
 
+const set1 = [
+    { title: 'Project 1', city: 'low', start: ' 9/1/2015', end: '9/3/2015' },
+]
+
+const set2 = [
+    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/1/2015' },
+    { title: 'Project 2', city: 'high', start: '9/2/2015', end: '9/6/2015' },
+    { title: 'Project 3', city: 'low', start: '9/6/2015', end: '9/8/2015' },
+]
+
+const set3 = [
+    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/3/2015' },
+    { title: 'Project 2', city: 'high', start: '9/5/2015', end: '9/7/2015' },
+    { title: 'Project 3', city: 'low', start: '9/8/2015', end: '9/8/2015' },
+]
+
+const set4 = [
+    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/1/2015' },
+    { title: 'Project 2', city: 'low', start: '9/1/2015', end: '9/1/2015' },
+    { title: 'Project 3', city: 'high', start: '9/2/2015', end: '9/2/2015' },
+    { title: 'Project 4', city: 'high', start: '9/2/2015', end: '9/3/2015' },
+]
+
+function formatDateString(dateString) {
+    return new Date(dateString).toLocaleDateString("en-US")
+}
+
+function getDatesBetweenRange(startDate, endDate, city) {
+    const dates = []
+    const currentDate = new Date(startDate)
+
+    if (currentDate >= new Date(endDate)) {
+        console.warn('no dates between');
+    }
+
+    while (currentDate < new Date(endDate)) {
+        dates.push([formatDateString(currentDate), city]) // match the data structure for calculation
+        currentDate.setDate(currentDate.getDate() + 1)
+    }
+
+    return dates;
+}
+
+
+
+
 /* 
 Format for calculation (parsing input)
 [[startDate, city],[betweenDates, city], [endDate, city], [startDate, city],[betweenDates, city], [endDate, city]...]
@@ -41,6 +85,26 @@ note:
 - each date should only occur once
 - if there is a gap between any dates, use to determine travel vs. full.
 */
+
+function formatSetForCalculation(setOfProjects) {
+    return setOfProjects
+        .reduce((acc, project) => {
+
+            // First just count every date from start to end
+            return [...acc,
+            [project.start, project.city],
+            ...getDatesBetweenRange(project.start, project.end, project.city),
+            [project.end, project.city]]
+
+        }, [])
+        .reduce((acc, day) => {
+
+            // Then, filter for unique dates.
+            if (acc.some(x => x[0] === day[0])) return acc;
+            return [...acc, day]
+
+        }, [])
+}
 
 /*
 Calculate from function
