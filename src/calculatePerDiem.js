62a82d079075903b7578fa677c148663fe557733
Andrew <awebb@ultralinq.com>
2022-02-09 12:15:02 -0500

    move example sets to their own file

--- src/calculatePerDiem.js
+++ src/calculatePerDiem.js
@@ -5,6 +5,7 @@ export function calculatePerDiem(set) {
 
 
 // Data structures
+// Could add  this to be configurable via app ( and add more cities via UI)
 const cities = [
     {
         title: 'low',
@@ -18,38 +19,6 @@ const cities = [
     },
 ]
 
-
-const set1 = [
-    { title: 'Project 1', city: 'low', start: ' 9/1/2015', end: '9/3/2015' },
-]
-
-const set2 = [
-    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/1/2015' },
-    { title: 'Project 2', city: 'high', start: '9/2/2015', end: '9/6/2015' },
-    { title: 'Project 3', city: 'low', start: '9/6/2015', end: '9/8/2015' },
-]
-
-const set3 = [
-    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/3/2015' },
-    { title: 'Project 2', city: 'high', start: '9/5/2015', end: '9/7/2015' },
-    { title: 'Project 3', city: 'high', start: '9/8/2015', end: '9/8/2015' },
-]
-
-const set4 = [
-    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/1/2015' },
-    { title: 'Project 2', city: 'low', start: '9/1/2015', end: '9/1/2015' },
-    { title: 'Project 3', city: 'high', start: '9/2/2015', end: '9/2/2015' },
-    { title: 'Project 4', city: 'high', start: '9/2/2015', end: '9/3/2015' },
-]
-
-// This set is a case where the first and last days should be full instead of travel because they're adjacent to gaps.
-// However, that case goes against the first rule: "First day and last day of a project, or sequence of projects, is a travel day."
-const set5 = [
-    { title: 'Project 1', city: 'low', start: '12/31/2015', end: '12/31/2015' },
-    { title: 'Project 2', city: 'high', start: '1/2/2016', end: '1/6/2016' },
-    { title: 'Project 3', city: 'high', start: '1/8/2016', end: '1/8/2016' },
-]
-
 function formatDateString(dateString) {
     return new Date(dateString).toLocaleDateString("en-US")
 }
@@ -59,7 +28,7 @@ function getDatesBetweenRange(startDate, endDate, city) {
     const currentDate = new Date(startDate)
 
     if (currentDate >= new Date(endDate)) {
-        console.warn('no dates between');
+        // console.warn('no dates between');
     }
 
     while (currentDate < new Date(endDate)) {
---
+++ src/sets.js
@@ -0,0 +1,32 @@
+const set1 = [
+    { title: 'Project 1', city: 'low', start: ' 9/1/2015', end: '9/3/2015' },
+]
+
+const set2 = [
+    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/1/2015' },
+    { title: 'Project 2', city: 'high', start: '9/2/2015', end: '9/6/2015' },
+    { title: 'Project 3', city: 'low', start: '9/6/2015', end: '9/8/2015' },
+]
+
+const set3 = [
+    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/3/2015' },
+    { title: 'Project 2', city: 'high', start: '9/5/2015', end: '9/7/2015' },
+    { title: 'Project 3', city: 'high', start: '9/8/2015', end: '9/8/2015' },
+]
+
+const set4 = [
+    { title: 'Project 1', city: 'low', start: '9/1/2015', end: '9/1/2015' },
+    { title: 'Project 2', city: 'low', start: '9/1/2015', end: '9/1/2015' },
+    { title: 'Project 3', city: 'high', start: '9/2/2015', end: '9/2/2015' },
+    { title: 'Project 4', city: 'high', start: '9/2/2015', end: '9/3/2015' },
+]
+
+// This set is a case where the first and last days should be full instead of travel because they're adjacent to gaps.
+// However, that case goes against the first rule: "First day and last day of a project, or sequence of projects, is a travel day."
+const set5 = [
+    { title: 'Project 1', city: 'low', start: '12/31/2015', end: '12/31/2015' },
+    { title: 'Project 2', city: 'high', start: '1/2/2016', end: '1/6/2016' },
+    { title: 'Project 3', city: 'high', start: '1/8/2016', end: '1/8/2016' },
+]
+
+export { set1, set2, set3, set4, set5 }
