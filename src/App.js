fd230235d184e37348cdd23a02560df348df6e1f
Andrew <awebb@ultralinq.com>
2022-02-09 12:15:32 -0500

    add core UI components and example functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { calculatePerDiem } from './calculatePerDiem';
+import { set1, set2, set3, set4 } from './sets';
+
+const sets = [
+  { id: 1, set: set1 },
+  { id: 2, set: set2 },
+  { id: 3, set: set3 },
+  { id: 4, set: set4 },
+]
+
+function SetButton({ id, text, setSet }) {
+  return (<button className='set-button' onClick={(e) => setSet(id)}>{text}</button>)
+}
+
 
 function App() {
+  const [currentSet, setCurrentSet] = useState([])
+  const [price, setPrice] = useState(0)
+
+  useEffect(() => {
+    setPrice(calculatePerDiem(currentSet))
+
+  }, [currentSet]);
+
+
+  const setSet = (id) => {
+    return setCurrentSet(sets.find(x => x.id === id).set)
+  }
+
   return (
     <div className="App">
-    </div>
+      <main>
+        <div className='title-bar'>
+          <h1 className='set-title'>Per Diem</h1>
+          <div className='price-container'><h2 className='price'>{`$${price}`}</h2></div>
+        </div>
+        {currentSet && currentSet.map(({ title, city, start, end }, index) => {
+          return (
+            <div key={index} className='project'>
+              <div className='project-info'>
+                <h5 className='project-title'>{title}</h5>
+                <p><strong>City:</strong> {city}</p>
+                <p><strong>Start:</strong> {start}</p>
+                <p><strong>End:</strong> {end}</p>
+              </div>
+            </div>
+          )
+        })}
+      </main>
+      <aside className='sidebar'>
+        <h2>Sets</h2>
+        <div className='line'></div>
+        <div className='set-buttons'>
+          {sets.map(({ id }) => <SetButton id={id} text={`Set ${id}`} setSet={setSet}></SetButton>)}
+        </div>
+      </aside>
+    </div >
   );
 }
 
